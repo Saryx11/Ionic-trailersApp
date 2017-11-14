1b908b908c0df175ddcb9727af66136c1a977ac9
Unknown <louis.benjamin@live.fr>
2017-11-14 16:14:41 +0100

    pages created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {NavController, Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import {LoginPage} from "../pages/login/login";
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild('myContent') nav: NavController
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
@@ -17,6 +19,10 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  LoginClick(){
+    this
   }
 }
 
