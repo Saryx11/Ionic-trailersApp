ec5955294d14bbc004a38ae82c2ca70dd543e572
Unknown <louis.benjamin@live.fr>
2017-11-14 16:45:51 +0100

    Menu Clicks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import {Component, ViewChild} from '@angular/core';
-import {NavController, Platform} from 'ionic-angular';
+import {NavController, Platform, Nav} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import {LoginPage} from "../pages/login/login";
+import {FavorisPage} from "../pages/favoris/favoris";
+import {PreferencesPage} from "../pages/preferences/preferences";
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild('myContent') nav: NavController
   rootPage:any = HomePage;
+  @ViewChild(Nav) nav: Nav;
+
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -20,9 +23,17 @@ export class MyApp {
       splashScreen.hide();
     });
   }
-
+  TrailersClick(){
+    this.nav.push(HomePage);
+  }
   LoginClick(){
-    this
+    this.nav.push(LoginPage);
+  }
+  FavorisClick(){
+    this.nav.push(FavorisPage);
+  }
+  PreferencesClick(){
+    this.nav.push(PreferencesPage);
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {LoginPage} from "../pages/login/login";
+import {PreferencesPage} from "../pages/preferences/preferences";
+import {TrailerPage} from "../pages/trailer/trailer";
+import {FavorisPage} from "../pages/favoris/favoris";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    PreferencesPage,
+    TrailerPage,
+    FavorisPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +27,11 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LoginPage,
+    PreferencesPage,
+    TrailerPage,
+    FavorisPage
   ],
   providers: [
     StatusBar,
